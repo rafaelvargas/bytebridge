9515a2d3479d4ecd50ffd5ab4a9d413255b5fb39
Rafael Vargas <contact@rafaelvargas.dev>
2023-11-11 13:53:31 -0300

    Add the initial code structure

---
+++ bytebridge/cli.py
@@ -0,0 +1,25 @@
+import argparse
+
+
+def _execute_transfer_operation(args):
+    print("The passed arguments were:", args)
+
+
+def main():
+    parser = argparse.ArgumentParser(description="ByteBridge CLI")
+    subparsers = parser.add_subparsers(dest="operation", title="Operations")
+
+    transfer_parser = subparsers.add_parser("transfer", help="Transfer data operation")
+    transfer_parser.add_argument("--source", required=True, help="Source data location")
+    transfer_parser.add_argument("--destination", required=True, help="Destination data location")
+    transfer_parser.add_argument("--query", required=True, help="Transformation query or script")
+    transfer_parser.add_argument("--map", required=True, help="Column mapping")
+
+    args = parser.parse_args()
+
+    if args.operation == "transfer":
+        _execute_transfer_operation(args)
+
+
+if __name__ == "__main__":
+    main()
---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="bytebridge",
+    version="0.0.1",
+    packages=find_packages(),
+    install_requires=[],
+    entry_points={
+        'console_scripts': [
+            'bytebridge=bytebridge.cli:main',
+        ],
+    },
+)
