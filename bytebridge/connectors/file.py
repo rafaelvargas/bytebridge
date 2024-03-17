691c06710c22b7b98278e31c16be201bfc3dbada
Rafael Vargas <contact@rafaelvargas.dev>
2024-03-17 09:07:34 -0300

    Implement CSV support

--- bytebridge/connectors/file.py
+++ bytebridge/connectors/file.py
@@ -3,6 +3,9 @@ from typing import Iterator, List
 import pyarrow
 from pyarrow import parquet
 
+import csv
+import itertools
+
 from .base import Connector
 
 
@@ -34,3 +37,39 @@ class ParquetConnector(Connector):
                 writer.write_batch(record_batch)
                 for batch in batch_iterator:
                     writer.write_batch(batch=pyarrow.RecordBatch.from_pylist(batch))
+
+
+class CsvConnector(Connector):
+    def __init__(self, *, connection_parameters: dict) -> None:
+        self._delimiter = connection_parameters.get("delimiter", ",")
+
+    def extract(
+        self,
+        *,
+        source_object: str,
+        source_query: str,
+        batch_size: int,
+    ) -> Iterator[List[dict]]:
+        with open(source_object, "r") as source_file:
+            csv_reader = csv.DictReader(source_file, delimiter=self._delimiter)
+            while batch := tuple(itertools.islice(csv_reader, batch_size)):
+                yield batch
+
+    def load(
+        self,
+        *,
+        target_object: str,
+        batch_iterator: Iterator[dict],
+    ):
+        first_batch = next(batch_iterator)
+        if first_batch:
+            with open(target_object, "w") as target_file:
+                csv_writer = csv.DictWriter(
+                    target_file,
+                    fieldnames=first_batch[0].keys(),
+                    delimiter=self._delimiter,
+                )
+                csv_writer.writeheader()
+                csv_writer.writerows(first_batch)
+                for batch in batch_iterator:
+                    csv_writer.writerows(batch)
--- bytebridge/operations.py
+++ bytebridge/operations.py
@@ -1,17 +1,20 @@
 from typing import Union
 
 from .connectors.database import MySqlConnector, PostgresConnector, MsSqlConnector
-from .connectors.file import ParquetConnector
+from .connectors.file import ParquetConnector, CsvConnector
 
 
 def _get_connector(
     connector_type: str
-) -> Union[PostgresConnector, MySqlConnector, ParquetConnector]:
+) -> Union[
+    PostgresConnector, MySqlConnector, ParquetConnector, MsSqlConnector, CsvConnector
+]:
     supported_connectors = {
         "postgresql": PostgresConnector,
         "mysql": MySqlConnector,
         "parquet": ParquetConnector,
         "mssql": MsSqlConnector,
+        "csv": CsvConnector,
     }
     if connector_type not in supported_connectors:
         raise ValueError(f"The connector {connector_type} is not supported.")
@@ -31,7 +34,7 @@ def transfer(
         connection_parameters=source_connection.get("parameters"),
     )
     destination_connector = _get_connector(destination_connection["type"])(
-        connection_parameters=destination_connection.get("parameters"),
+        connection_parameters=destination_connection.get("parameters", {}),
     )
     batch_iterator = source_connector.extract(
         source_query=source_query,
